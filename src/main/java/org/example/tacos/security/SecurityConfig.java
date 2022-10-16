8d8bb7d0723d459bc598f9c93e02c12b24ddb8e9
Anton <antonsvetlovsky@gmail.com>
2022-10-16 22:31:58 +0300

    Add logout

--- src/main/java/org/example/tacos/security/SecurityConfig.java
+++ src/main/java/org/example/tacos/security/SecurityConfig.java
@@ -4,45 +4,12 @@ import org.example.tacos.dao.UserRepository;
 import org.example.tacos.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.inMemoryAuthentication()
-//                .withUser("buzz")
-//                    .password("{noop}infinity")
-//                    .authorities("ROLE_USER")
-//                .and()
-//                .withUser("woody")
-//                    .password("{noop}bullseye")
-//                    .authorities("ROLE_USER");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.headers().frameOptions().disable();
-//    }
-//}
 
 @Configuration
 public class SecurityConfig {
@@ -51,22 +18,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-//        List<UserDetails> userList = new ArrayList<>();
-//        userList.add(new User(
-//                "buzz",
-//                encoder.encode("password"),
-//                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))
-//        ));
-//        userList.add(new User(
-//                "woody",
-//                encoder.encode("password"),
-//                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))
-//        ));
-//        return new InMemoryUserDetailsManager(userList);
-//    }
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
@@ -88,7 +39,9 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/design")
+                .defaultSuccessUrl("/design", true)
+                .and()
+                .logout()
                 .and()
                 .build();
     }
